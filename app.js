d0d6147a48edd3bfc3369319b3d0fb519ea6bc23
Aditya More <68657272+Adi720@users.noreply.github.com>
2021-03-12 23:08:18 +0530

    Routes Update

--- app.js
+++ app.js
@@ -25,6 +25,8 @@ const Article = mongoose.model("Article", articleSchema);
 
 app.route("/articles")
 
+//                            Request Targetting all articles                              //
+
 
 .get(function(req, res){
   Article.find(function(err, articles){
@@ -64,6 +66,66 @@ app.route("/articles")
 
 });
 
+//                       Request Targeting Specific Article                         //
+
+app.route("/articles/:articleTitle")
+
+.get(function(req, res){
+  Article.findOne({title: req.params.articleTitle}, function(err, foundArticle){
+    if(foundArticle){
+      res.send(foundArticle);
+    }
+    else{
+      res.send("No articles matching that title was found.")
+    }
+  })
+
+})
+
+.put(function(req, res){
+
+  const articleTitle = req.params.articleTitle;
+
+  Article.updateOne(
+    {title: req.params.articleTitle},
+    {title: req.body.title ,content: req.body.content},
+    {overwrite: true},
+    function(err){
+      if (!err){
+        res.send("Successfully updated content of the selected article.");
+      } else {
+        res.send(err);
+      }
+    });
+})
+
+.patch(function(req, res){
+  const articleTitle = req.params.articleTitle;
+  Article.updateOne(
+    {title: req.params.articleTitle},
+    {content: req.body.content},
+    function(err){
+      if (!err){
+        res.send("Successfully updated selected article.");
+      } else {
+        res.send(err);
+      }
+    });
+})
+
+.delete(function(req,res){
+  Article.deleteOne(
+    {title: req.params.articleTitle},
+    function(err){
+      if(!err){
+        res.send("Successfully deleted the article")
+      }
+      else{
+        res.send(err);
+      }
+    }
+  )
+});
 
 
 app.listen(3000, function () {
